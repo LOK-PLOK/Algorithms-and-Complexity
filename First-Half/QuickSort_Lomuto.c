3d9de04f5a404385d052556777f58297db239878
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2025-02-07 09:48:44 +0800

    fix: partition function and proper naming

--- First-Half/QuickSort_Lomuto.c
+++ First-Half/QuickSort_Lomuto.c
@@ -1,9 +1,10 @@
 #include "header/utils.c"
 
 void quickSort(int *, int, int);
+int lomutoPartition(int*,int,int);
 
 int main(void) {
-  system("cls");
+  system("clear");
   int array[6] = {10, 7, 8, 9, 1, 5};
   int size = 6;
   printf("Before quicksort: ");
@@ -11,35 +12,33 @@ int main(void) {
   quickSort(array, 0, size - 1);
   printf("\nAfter quicksort: ");
   printArr(array, size);
+  printf("\n\n");
 
   return 0;
 }
 
-void quickSort(int arr[], int low, int high) {  //  Recursive
-// check whether if low (MinIndx) it still less than high (MaxIndx)
+void quickSort(int arr[], int low, int high) {  
   if(low < high){ 
-    // assign the last element as the pivot element
-    int pi = arr[high];
-    // getting the partition index, should be -1 at the very first call of the function
-    int i = low - 1, j;
-
-    for(j = low; j < high; j++){
-      if(arr[j] <= pi){
-        i++;
-        int temp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = temp;
-      }
-    }
-    
-    // we swap the pivot element to its proper spot/index
-    i++;
-    int temp2 = arr[i];
-    arr[i] = arr[high];
-    arr[high] = temp2;
-
-    //sorting left parition and right partiion
-    quickSort(arr,low,i-1); // Left
-    quickSort(arr,i+1,high);// Right
+    int pi = lomutoPartition(arr,low,high);
+    quickSort(arr,low,pi-1); 
+    quickSort(arr,pi+1,high);
   }
+}
+
+int lomutoPartition(int arr[],int low,int high){
+  int pivot = arr[high];
+  int i = low - 1,j,temp = 0;
+
+  for(j=low;j<high;j++){
+    if(arr[j] <= pivot){
+      temp = arr[++i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  temp = arr[++i];
+  arr[i] = arr[high];
+  arr[high] = temp;
+
+  return i;
 }
