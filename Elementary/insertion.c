3591d56d25862d942045400e9d972b4c503190e2
Paul France M. Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2025-02-21 20:42:10 +0800

    feat: finally understood insertion sort HAHA

--- Elementary/insertion.c
+++ Elementary/insertion.c
@@ -10,6 +10,17 @@ void insertionSortSwap(int arr[], int size) {
     }
 }
 
+void insertionSortShit(int arr[], int size) {
+    int i,j,key;
+    for(i = 1; i< size;i++){
+        key = arr[i];
+        for(j = i-1; j >= 0 && arr[j] > key; j--){
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = key;
+    }
+}
+
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
