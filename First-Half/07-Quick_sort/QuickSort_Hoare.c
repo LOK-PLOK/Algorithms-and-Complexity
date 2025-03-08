434e19c494fa4fc197cf20c2394773c7c35f2c04
Paul France M. Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2025-03-08 09:03:27 +0800

    feat: added 'QuickSort Lomuto version' sorting algorithm

--- First-Half/07-Quick_sort/QuickSort_Hoare.c
+++ First-Half/07-Quick_sort/QuickSort_Hoare.c
@@ -3,6 +3,7 @@
 
 void quickSort(int arr[], int low, int high);
 void quickSortWrapper(int arr[], int size);
+int lomuto(int arr[],int low,int high);
 
 int main(void){
   
@@ -11,25 +12,47 @@ int main(void){
 }
 
 void quickSortWrapper(int arr[], int size) {
-    quickSort(arr, 0, size - 1);
+  quickSort(arr, 0, size - 1);
 }
 
 void quickSort(int arr[], int low, int high) {  
-  if (low < high) {
-      int i = low - 1;
-      int j = low;
-      int pivot = arr[high];
+  if(low < high){
+    int i = low-1,j=low,temp;
+    int pivot = arr[high];
 
-      for (; j < high; j++) {
-          if (arr[j] < pivot) {
-              swap(arr + (++i), arr + j);
-          }
+    for(;j<high;j++){
+      if(arr[j] <= pivot){
+        i++;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
       }
+    }
+    i++;
+    temp = arr[i];
+    arr[i] = arr[high];
+    arr[high] = temp;
 
-      swap(arr + (++i), arr + high);
-
-      quickSort(arr, low, i - 1);
-      quickSort(arr, i + 1, high);
+    quickSort(arr,low,i-1);
+    quickSort(arr,i+1,high);
   }
 }
 
+// int lomuto(int arr[],int low,int high){
+//   int pivot = arr[high];
+//     int i = low - 1;
+
+//     for (int j = low; j < high; j++) {
+//         if (arr[j] <= pivot) {
+//             i++;
+//             int temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+//     i++;
+//     int temp = arr[i];
+//     arr[i] = arr[high];
+//     arr[high] = temp;
+//     return i;
+// }
