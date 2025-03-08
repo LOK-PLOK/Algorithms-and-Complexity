3b18ac480cc89ff7f77506a7f9e3be9c74875554
Paul France M. Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2025-03-08 12:39:32 +0800

    feat: added heapsort 'heapify subtree recusive version'

--- First-Half/04-Heap_sort/heap.c
+++ First-Half/04-Heap_sort/heap.c
@@ -3,6 +3,7 @@
 
 void heapSort(int arr[], int n);
 void heapifySubtree(int arr[],int lastNdx,int parent);
+void heapifySubtreeRec(int arr[],int lastNdx,int parent);
 
 int main(void){
     
@@ -12,7 +13,7 @@ int main(void){
 
 void heapSort(int arr[], int size){
     int i,lastndx = size-1,temp;
-
+    
     for(i = (lastndx-1)/2; i>=0; i--){
         heapifySubtree(arr,lastndx,i);
     }
@@ -27,20 +28,20 @@ void heapSort(int arr[], int size){
 
 void heapifySubtree(int arr[],int lastNdx,int parent){
     int big,LC,RC,flag=0,temp;
-
+    
     while(!flag){
         big = parent;
         LC = parent*2+1;
         RC = LC + 1;
-
+        
         if(LC <= lastNdx && arr[LC] > arr[big]){
             big = LC;
         }
-
+        
         if(RC <= lastNdx && arr[RC] > arr[big]){
             big = RC;
         }
-
+        
         if(big != parent){
             temp = arr[big];
             arr[big] = arr[parent];
@@ -50,4 +51,26 @@ void heapifySubtree(int arr[],int lastNdx,int parent){
             flag = 1;
         }
     }
+}
+
+void heapifySubtreeRec(int arr[],int lastNdx,int parent){
+    int big = parent,temp;
+    int LC = parent*2+1;
+    int RC = LC + 1;
+
+    if(LC <= lastNdx && arr[LC] > arr[big]){
+        big = LC;
+    }
+
+    if(RC <= lastNdx && arr[RC] > arr[big]){
+        big = RC;
+    }
+
+    if(big != parent){
+        temp = arr[big];
+        arr[big] = arr[parent];
+        arr[parent] = temp;
+        heapifySubtreeRec(arr,lastNdx,big);
+    }
+
 }
