a02cf2b0517b7011ce6ead5ed4f4c8771d782314
Paul France M. Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2025-03-09 19:50:34 +0800

    feat: added radixsort 'LSD and MSD version' sorting algorithm

---
+++ First-Half/10-Radix_sort/MSD.c
@@ -0,0 +1,53 @@
+#include "../../utilities/general.h"
+#include "../../utilities/test.h"
+#include <string.h> // Include string.h for memcpy
+
+#define BASE 10
+
+void radixSort(int arr[], int n);
+
+int main(void) {
+    test_sort_w_size(radixSort);
+    return 0;
+}
+
+void radixSort(int arr[], int size) {
+    int i, max = arr[0], exp;
+
+    // Find the maximum number to know the number of digits
+    for (i = 1; i < size; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+
+    // Do counting sort for every digit. Note that instead of passing the digit number, exp is passed. exp is 10^i where i is the current digit number
+    for (exp = 1; max / exp > 0; exp *= 10) {
+        int output[size]; // Output array
+        int count[BASE] = {0}; // Initialize count array with all zeros
+
+        // Store count of occurrences in count[]
+        for (i = 0; i < size; i++) {
+            count[(arr[i] / exp) % BASE]++;
+        }
+
+        // Change count[i] so that count[i] now contains the actual position of this digit in output[]
+        for (i = 1; i < BASE; i++) {
+            count[i] += count[i - 1];
+        }
+
+        // Build the output array
+        for (i = size - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % BASE] - 1] = arr[i];
+            count[(arr[i] / exp) % BASE]--;
+        }
+
+        // Copy the output array to arr[], so that arr[] now contains sorted numbers according to the current digit
+        for (i = 0; i < size; i++) {
+            arr[i] = output[i];
+        }
+
+        // or use memcpy
+        // memcpy(arr, output, sizeof(int) * size);
+    }
+}
